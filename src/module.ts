ba8c550aec2d300c58bc793fe0ee405ce9b7793f
nightrunner4 <118369464+nightrunner4@users.noreply.github.com>
2023-12-27 16:43:36 +0100

    Initial vite & ts project

---
+++ src/module.ts
@@ -0,0 +1,5 @@
+function sayHello() {
+  console.log("hello");
+}
+
+export default sayHello;
---
+++ src/vite-env.d.ts
@@ -0,0 +1,3 @@
+// This file extends or overrides TypeScript types for Vite, allowing custom type declarations and augmentations.
+
+/// <reference types="vite/client" />
