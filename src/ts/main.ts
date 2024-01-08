0b272042ac2651876c64cb5a803f4cb3e4301c20
nightrunner4 <118369464+nightrunner4@users.noreply.github.com>
2024-01-08 11:43:39 +0100

    Create PlayerRenderer class
    Where all the rendering DOM manipulation methods are encapsulated,
    improving readability and organization

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -4,122 +4,152 @@ import "../styles/style.css";
 import { Name, Orientation, Ship } from "./modules/ship";
 import { Gameboard, Cell } from "./modules/gameboard";
 
-// Add drag & drop functionality
-function addDragAndDrop(): void {
-  document.addEventListener("dragstart", (event: DragEvent) => {
-    // Get the dragged ship
-    const draggedShip = (event.target as HTMLElement).className;
-    if (event.dataTransfer) {
-      event.dataTransfer.setData("text/plain", draggedShip);
-    }
-  });
+class Player {
+  name: string;
+  gameboard: Gameboard;
+  death: boolean;
 
-  document.addEventListener("dragover", (event: DragEvent) => {
-    event.preventDefault();
-  });
+  constructor(name: string, gameboard: Gameboard) {
+    this.name = name;
+    this.gameboard = gameboard;
+    this.death = false;
+  }
+}
 
-  // Find the ship and coordinates in which it has been droped
-  // Update the gameboard
-  // Render the updated ship
-  document.addEventListener("drop", (event: DragEvent) => {
-    event.preventDefault();
+class PlayerRenderer {
+  player: Player;
+  boardContainer: HTMLElement;
 
-    // Get the ship that has been dropped
-    if (event.dataTransfer) {
-      const droppedClass = event.dataTransfer.getData("text/plain");
-      const droppedShip = playerGameboardObj.ships.find((ship) => ship.name === droppedClass);
+  constructor(player: Player) {
+    this.player = player;
+    this.boardContainer = document.createElement("div");
+    this.boardContainer.className = this.player.name;
+  }
 
-      if (droppedShip) {
-        // Find the position in which the ship has been dropped
-        const dropCell = (event.target as HTMLElement).closest(".grid-item");
+  createBoard() {
+    const boardContainer = document.createElement("div");
+    boardContainer.className = this.player.name;
 
-        if (dropCell) {
-          // Update the gameboard with the new position
-          const [x, y] = JSON.parse(dropCell.id);
-          playerGameboardObj.moveShip(droppedShip, [x, y]);
+    for (let row = 0; row < 10; row++) {
+      for (let col = 0; col < 10; col++) {
+        // Create div
+        const boardCell = document.createElement("div");
+        boardCell.className = this.player.name + "-cell";
 
-          // Render updated ship
-          renderShip(droppedShip, playerGameboardHTML);
-        }
+        // Set the id of the div to be its coordinates
+        const coordinates: [number, number] = [col, row];
+        boardCell.id = JSON.stringify(coordinates);
+
+        this.boardContainer.appendChild(boardCell);
       }
     }
-  });
-}
 
-// Create a 10x10 gameboard
-function createBoard(gameboard: HTMLElement) {
-  for (let row = 0; row < 10; row++) {
-    for (let col = 0; col < 10; col++) {
-      // Create div
-      const gridItem = document.createElement("div");
-      gridItem.className = "grid-item";
+    document.body.appendChild(this.boardContainer);
+  }
 
-      // Set the id of the div to be its coordinates
-      const coordinates: [number, number] = [col, row];
-      gridItem.id = JSON.stringify(coordinates);
+  renderAttacks() {
+    const board = this.player.gameboard.board;
 
-      gameboard.appendChild(gridItem);
+    // Flatten the board matrix column-wise
+    const transposedBoard = board[0].map((_, i) => board.map((row) => row[i]));
+    const boardList = transposedBoard.flat();
+
+    // Select html grid items inside specified gameboard
+    var htmlCells = this.boardContainer.getElementsByClassName(this.player.name + "-cell");
+
+    // Iterate through board list, add 🔥 to the corresponding html divs
+    boardList.forEach((objCell, index) => {
+      if (objCell.hit && objCell.ship) {
+        htmlCells[index].textContent = "🔥";
+      }
+    });
+  }
+
+  renderShips() {
+    function renderShip(ship: Ship, gameboard: HTMLElement) {
+      const [x, y] = ship.position;
+
+      // Remove existing ship with the same name
+      const existingShip = document.querySelector(`.${ship.name}`);
+      if (existingShip) {
+        existingShip.remove();
+      }
+
+      // Create ship div
+      const shipDiv = document.createElement("div");
+      shipDiv.classList.add(ship.name);
+      shipDiv.setAttribute("draggable", "true");
+      gameboard.appendChild(shipDiv);
+
+      // Find pixel coordinates of ship
+      const cellSize = 50; // px
+      const topValue = `${y * cellSize}px`;
+      const leftValue = `${x * cellSize}px`;
+
+      // Add dimensions to ship
+      if (ship.orientation === "Horizontal") {
+        shipDiv.style.width = `${ship.length * cellSize}px`;
+        shipDiv.style.height = `${cellSize}px`;
+      } else {
+        shipDiv.style.width = `${cellSize}px`;
+        shipDiv.style.height = `${ship.length * cellSize}px`;
+      }
+
+      // Position ship
+      shipDiv.style.top = topValue;
+      shipDiv.style.left = leftValue;
     }
-  }
-}
 
-// Render a ship object on a gameboard
-function renderShip(ship: Ship, gameboard: HTMLElement) {
-  const [x, y] = ship.position;
-
-  // Remove existing ship with the same name
-  const existingShip = document.querySelector(`.${ship.name}`);
-  if (existingShip) {
-    existingShip.remove();
+    this.player.gameboard.ships.forEach((ship) => renderShip(ship, this.boardContainer));
   }
 
-  // Create ship div
-  const shipDiv = document.createElement("div");
-  shipDiv.classList.add(ship.name);
-  shipDiv.setAttribute("draggable", "true");
-  gameboard.appendChild(shipDiv);
+  addDragDrop(): void {
+    this.boardContainer.addEventListener("dragstart", (event: DragEvent) => {
+      // Get the dragged ship
+      const draggedShip = (event.target as HTMLElement).className;
+      if (event.dataTransfer) {
+        event.dataTransfer.setData("text/plain", draggedShip);
+      }
+    });
 
-  // Find pixel coordinates of ship
-  const cellSize = 50; // px
-  const topValue = `${y * cellSize}px`;
-  const leftValue = `${x * cellSize}px`;
+    document.addEventListener("dragover", (event: DragEvent) => {
+      event.preventDefault();
+    });
 
-  // Add dimensions to ship
-  if (ship.orientation === "Horizontal") {
-    shipDiv.style.width = `${ship.length * cellSize}px`;
-    shipDiv.style.height = `${cellSize}px`;
-  } else {
-    shipDiv.style.width = `${cellSize}px`;
-    shipDiv.style.height = `${ship.length * cellSize}px`;
+    // Find the ship and coordinates in which it has been droped
+    // Update the gameboard
+    // Render the updated ship
+    document.addEventListener("drop", (event: DragEvent) => {
+      event.preventDefault();
+
+      // Get the ship that has been dropped
+      if (event.dataTransfer) {
+        const droppedClass = event.dataTransfer.getData("text/plain");
+        const droppedShip = this.player.gameboard.ships.find((ship) => ship.name === droppedClass);
+
+        if (droppedShip) {
+          // Find the position in which the ship has been dropped
+          const dropCell = (event.target as HTMLElement).closest(`.${this.player.name}-cell`);
+
+          if (dropCell) {
+            // Update the gameboard with the new position
+            const [x, y] = JSON.parse(dropCell.id);
+            this.player.gameboard.moveShip(droppedShip, [x, y]);
+
+            // Render updated ship
+            this.renderShips();
+          }
+        }
+      }
+    });
   }
-
-  // Position ship
-  shipDiv.style.top = topValue;
-  shipDiv.style.left = leftValue;
-}
-
-// Render board matrix
-function renderBoard(board: Cell[][], gameboardHTML: HTMLElement): void {
-  // Flatten the board matrix column-wise
-  const transposedBoard = board[0].map((_, i) => board.map((row) => row[i]));
-  const boardList = transposedBoard.flat();
-
-  // Select html grid items inside specified gameboard
-  var htmlCells = gameboardHTML.getElementsByClassName("grid-item");
-
-  // Iterate through board list, add 🔥 to the corresponding html divs
-  boardList.forEach((objCell, index) => {
-    if (objCell.hit && objCell.ship) {
-      htmlCells[index].textContent = "🔥";
-    }
-  });
 }
 
 // Program starts
 
-// Create data
+// Create gameboard data
 
-const playerGameboardObj = new Gameboard();
+const playerGameboardObj: Gameboard = new Gameboard();
 
 const destroyer = new Ship(Name.Destroyer, [5, 5], Orientation.Horizontal);
 const carrier = new Ship(Name.Carrier, [3, 3], Orientation.Vertical);
@@ -136,19 +166,17 @@ playerGameboardObj.createAttack([3, 3]);
 playerGameboardObj.createAttack([0, 2]);
 playerGameboardObj.createAttack([7, 9]);
 
-// Create HTML board
+// Create player data
 
-const playerGameboardHTML = document.querySelector(".gameboard") as HTMLElement;
-createBoard(playerGameboardHTML);
+const player1: Player = new Player("Player1", playerGameboardObj);
 
-// Render board
+// Create PlayerRenderer
 
-renderBoard(playerGameboardObj.board, playerGameboardHTML);
+const playerRenderer1: PlayerRenderer = new PlayerRenderer(player1);
 
-// Render ships
+// Render
 
-playerGameboardObj.ships.forEach((ship) => renderShip(ship, playerGameboardHTML));
-
-// Drag & Drop functionality
-
-addDragAndDrop();
+playerRenderer1.createBoard();
+playerRenderer1.renderAttacks();
+playerRenderer1.renderShips();
+playerRenderer1.addDragDrop();
