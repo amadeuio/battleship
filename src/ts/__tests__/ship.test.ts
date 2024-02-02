e50aaf7015a87aa06d476ab5806ebc84dc8c19f0
nightrunner4 <118369464+nightrunner4@users.noreply.github.com>
2024-02-02 17:57:38 +0100

    Refactor ship tests by encapsulating in describe blocks

--- src/ts/__tests__/ship.test.ts
+++ src/ts/__tests__/ship.test.ts
@@ -7,66 +7,76 @@ describe("Ship Class", () => {
     ship = new Ship(Name.Carrier, [0, 0], Orientation.Horizontal);
   });
 
-  it("should be created with the correct properties", () => {
-    expect(ship.name).toBe(Name.Carrier);
-    expect(ship.length).toBe(5); // Carrier length
-    expect(ship.position).toEqual([0, 0]);
-    expect(ship.orientation).toBe(Orientation.Horizontal);
-    expect(ship.hits).toBe(0);
-    expect(ship.sunk).toBe(false);
+  describe("Ship", () => {
+    it("should be created with the correct properties", () => {
+      expect(ship.name).toBe(Name.Carrier);
+      expect(ship.length).toBe(5); // Carrier length
+      expect(ship.position).toEqual([0, 0]);
+      expect(ship.orientation).toBe(Orientation.Horizontal);
+      expect(ship.hits).toBe(0);
+      expect(ship.sunk).toBe(false);
+    });
   });
 
-  it("should hit the ship correctly", () => {
-    ship.hit();
-    expect(ship.hits).toBe(1);
-    expect(ship.sunk).toBe(false);
-
-    for (let i = 1; i < ship.length; i++) {
+  describe("hit", () => {
+    it("should increase the hit property correctly", () => {
       ship.hit();
-    }
+      expect(ship.hits).toBe(1);
+      expect(ship.sunk).toBe(false);
 
-    expect(ship.hits).toBe(ship.length);
-    expect(ship.sunk).toBe(true);
+      for (let i = 1; i < ship.length; i++) {
+        ship.hit();
+      }
+
+      expect(ship.hits).toBe(ship.length);
+      expect(ship.sunk).toBe(true);
+    });
   });
 
-  it("should check if the ship is sunk", () => {
-    expect(ship.isSunk()).toBe(false);
+  describe("isSunk", () => {
+    it("should check if the ship is sunk", () => {
+      expect(ship.isSunk()).toBe(false);
 
-    for (let i = 0; i < ship.length; i++) {
+      for (let i = 0; i < ship.length; i++) {
+        ship.hit();
+      }
+
+      expect(ship.isSunk()).toBe(true);
+    });
+  });
+
+  describe("get coordinates", () => {
+    it("should return correct coordinates", () => {
+      const coordinates = ship.coordinates;
+
+      expect(coordinates.length).toBe(ship.length);
+
+      for (let i = 0; i < ship.length; i++) {
+        expect(coordinates[i]).toEqual([0 + i, 0]);
+      }
+
+      // Test for a ship with vertical orientation
+      const verticalShip = new Ship(Name.Battleship, [3, 3], Orientation.Vertical);
+      const verticalCoordinates = verticalShip.coordinates;
+
+      expect(verticalCoordinates.length).toBe(verticalShip.length);
+
+      for (let i = 0; i < verticalShip.length; i++) {
+        expect(verticalCoordinates[i]).toEqual([3, 3 + i]);
+      }
+    });
+  });
+
+  describe("clone", () => {
+    it("should clone the ship correctly", () => {
       ship.hit();
-    }
+      const clonedShip = ship.clone();
 
-    expect(ship.isSunk()).toBe(true);
-  });
+      // Verify they have equal properties
+      expect(clonedShip).toEqual(ship);
 
-  it("should return correct coordinates", () => {
-    const coordinates = ship.coordinates;
-
-    expect(coordinates.length).toBe(ship.length);
-
-    for (let i = 0; i < ship.length; i++) {
-      expect(coordinates[i]).toEqual([0 + i, 0]);
-    }
-
-    // Test for a ship with vertical orientation
-    const verticalShip = new Ship(Name.Battleship, [3, 3], Orientation.Vertical);
-    const verticalCoordinates = verticalShip.coordinates;
-
-    expect(verticalCoordinates.length).toBe(verticalShip.length);
-
-    for (let i = 0; i < verticalShip.length; i++) {
-      expect(verticalCoordinates[i]).toEqual([3, 3 + i]);
-    }
-  });
-
-  it("should clone the ship correctly", () => {
-    ship.hit();
-    const clonedShip = ship.clone();
-
-    // Verify they have equal properties
-    expect(clonedShip).toEqual(ship);
-
-    // Verify the clone is a different instance (reference inequality)
-    expect(clonedShip).not.toBe(ship);
+      // Verify the clone is a different instance (reference inequality)
+      expect(clonedShip).not.toBe(ship);
+    });
   });
 });
